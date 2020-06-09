8a4000f7410b0607a407a56d925507f4ba86c41c
unknown <israelcarneiro97@gmail.com>
2020-06-09 02:09:58 -0300

    projeto cronometro pronto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+
+	constructor(props){
+		super(props);
+		this.state = {
+			numero: 0,
+			botaoInicio: "INICIO",
+		}
+		this.inicio = this.inicio.bind(this);
+		this.limpar = this.limpar.bind(this);
+		this.timer = null;
+	}
+
+	inicio(){
+		let state = this.state;
+		if(this.timer !== null){
+			clearInterval(this.timer);
+			this.timer = null;
+			state.botaoInicio = "INICIO";
+		}else{
+			this.timer = setInterval(() => {
+				state.numero += 0.1;
+				this.setState(state);
+			}, 100);
+			state.botaoInicio = "PAUSAR";
+		}
+		//this.setState(state);
+	}
+
+	limpar(){
+		if(this.timer !== null){
+			clearInterval(this.timer);
+			this.timer = null;
+		}
+
+		let state = this.state;
+		state.numero = 0;
+		state.botaoInicio = "INICIO";
+		this.setState(state);
+	}
+
+	render(){
+		return(
+			<div className="container">
+				<img className="img" src={require('./assets/cronometro.png')} alt=""/>
+				<a className="timer">{this.state.numero.toFixed(1)}</a>
+				<div className="areaBtn">
+					<a className="btn" onClick={this.inicio}>{this.state.botaoInicio}</a>
+					<a className="btn" onClick={this.limpar}>ZERAR</a>
+				</div>
+			</div>
+		);
+	}
 }
-
-export default App;
